ec8244758728625aac218113b154268163ddb267
David Alienyi <alienyidavid4christ@gmail.com>
2020-07-04 14:03:59 +0100

    update production config data

--- Config/Config.php
+++ Config/Config.php
@@ -10,11 +10,11 @@
             ),
     
             'production' => array(
-                'user' => 'root',
-                'password' => 'root',
-                'host' => '127.0.0.1',
-                'database' => 'bet_community',
-                'port' => 8889
+                'user' => 'nextgbec_user1',
+                'password' => 'Iloveodunayo123',
+                'host' => 'localhost',
+                'database' => 'nextgbec_bet_community',
+                'port' => 3306
             )
         );
     }
